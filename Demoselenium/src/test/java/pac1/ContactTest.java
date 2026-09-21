117ab6d7586216b5ec84b9f10554c9b54fd72a44
rifaz <rifaz@mohammedrifaz.mshome.net>
2026-09-21 15:25:13 +0530

    new frame work

---
+++ Demoselenium/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,19 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createcontactTest() {
+		System.out.println("execute create contct test");
+	}
+	
+	@Test
+	public void modifycontactTest() {
+		System.out.println("execute  modifycontactTest");
+	}
+	
+	
+
+}

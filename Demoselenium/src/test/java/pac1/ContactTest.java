00fac8f92be998f1e002e181b0a1be2161e6ad58
rifaz <rifaz@mohammedrifaz.mshome.net>
2026-09-23 10:50:38 +0530

    added new line

--- Demoselenium/src/test/java/pac1/ContactTest.java
+++ Demoselenium/src/test/java/pac1/ContactTest.java
@@ -6,6 +6,17 @@ public class ContactTest {
 	
 	@Test
 	public void createcontactTest() {
+		
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
 		System.out.println("execute create contct test");
 	}
 	
